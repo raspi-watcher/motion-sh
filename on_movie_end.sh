665b9804314a3c7394bf9f095798a2c4a3b93dd8
pinocchio <unzoning@gmail.com>
2015-09-03 17:52:58 +0900

    add removing line to on_movie_end.sh

--- on_movie_end.sh
+++ on_movie_end.sh
@@ -9,6 +9,8 @@ CHANNEL=camera
 TYPE=gif
 FILE_PATH=${DIR}gif/out.gif
 
+rm -f ${DIR}frames/*.png
 ffmpeg -i $1 ${DIR}frames/out%03d.png 2> ${DIR}${EVENT_TYPE}.log
 convert -loop 0 ${DIR}frames/out*.png ${DIR}gif/out.gif 2> ${DIR}${EVENT_TYPE}.log
 $SLACKCAT -k $SLACK_TOKEN -c $CHANNEL -t $TYPE -m $FILE_PATH 2> ${DIR}${EVENT_TYPE}.log
+
